f681db075f1b7814363ee05920f36b8eee5e3561
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-14 17:28:38 +0000

    populating drop down menu of currencies complete

--- src/app.js
+++ src/app.js
@@ -4,10 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      currencies: {},
+      // currencyKeys: []
+    },
+    mounted: function() {
+      this.fetchCurrencies()
     },
     methods: {
+      fetchCurrencies: function () {
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => this.currencies = data)
+        // .then(keyData => this.getKeys())
+        .then(console.log(this.currencies))
 
+      },
+      selectedCurrency: function () {
+
+      }
+      // getKeys: function () {
+      //   Object.keys(this.currencies.rates).forEach(function(key) {
+      //     this.currencyKeys.push(key)
+      //   })
+      // }
     }
   });
 });
